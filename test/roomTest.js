6c4e0514ff0b241f626f998a59914491229eaed6
pbernier42 <pacomebernier@orange.fr>
2021-08-19 15:31:51 +0200

    [pbernier] - Join room - create room pushed too the EXTREME

--- test/roomTest.js
+++ test/roomTest.js
@@ -1,82 +1,93 @@
 import Master from '../server/classes/Master.mjs';
 var master = new Master();
-//var expect = require('chai').expect
-import { getGameFromPlayerId } from './utils.mjs';
+
 import { expect } from 'chai';
+
+import { expectNewRoom, expectJoinRoom, getGameFromPlayerId } from './utils.mjs';
 import { defaultRules } from '../src/ressources/rules.mjs'
 
 //Nom du fichier dans lequel se trouves les fonctions testé
-describe('!! ', () => {
+describe('Server Tests', () => {
+
 	var cb = () => {
 		console.log('Callback');
 	};
+	const playersId = [ 475, 307, 18446744073709551615 ];
+	const players = [{ name: 'Hector' }, { name: '\t\n\r\v\f' }, { name: 'pouayayay' } ];
+	let room = {};
+	let badRoom = {}
 	//let sock = openSocket('http://localhost:8000')
+
 	//Nom de la fonction a tester
 	describe('[ROOM TESTS]', () => {
-		const playersId = [ 475, 307, 888 ];
-		const players = [ { name: 'Hector' }, { name: 'maisquoiOMG' }, { name: 'pouayayay' } ];
-		let room = {};
-
 		//Explication de se que devrait retournner la fonction
 		it('Devrait créer la room', () => {
+			//Nom: normal | Id: normal
 			master.createRoom(playersId[0], players[0], cb);
-			room = getGameFromPlayerId(475, master);
-      console.log(room)
-			// console.log(room)
-			//allRooms.prototype.forEach((game) => { console.log(game) })
-
-			//console.log(allRooms)
-			//console.log(allRooms.map((data) => { console.log(data) }))
-      expect(room.isInGame()).to.be.eql(false)
-      expect(room).to.exist;
-      expect(room.getNbPlayer()).to.be.eql(1)
-      expect(room.getRules()).to.be.eql(defaultRules)
-      expect(room.getInterval()).to.be.eql(undefined)
-      expect(room.getShapes()).to.be.eql([])
-      expect(room.getShapesId()).to.be.eql([])
-      expect(room.getReadyToStart()).to.be.eql({})
-      expect(room.getReadyToStart()).to.be.eql({})
-      expect(room.getOwner()).to.be.eql(playersId[0])
-      expect(room.getArrivalOrder()).to.be.eql([ playersId[0] ])
-			//expect(room._nbPlayer).to.equal(1)
-			//Ajouter des vérification a game.
-
-			//expect(master.createRoom(472, { name: "\t\n\r\v\f" }, cb)).to.exist
-			//expect(master.createRoom(18446744073709551615, { name: "Hector" }, cb)).to.exist
+			room = getGameFromPlayerId(playersId[0], master);
+      expectNewRoom(room, playersId[0])
+			//Nom: tnrvf | Id: normal
+			master.createRoom(playersId[1], players[1], cb);
+      expectNewRoom(getGameFromPlayerId(playersId[1], master), playersId[1]);
+			//Nom: normal | Id: long
+			master.createRoom(playersId[2], players[2], cb);
+      expectNewRoom(getGameFromPlayerId(playersId[2], master), playersId[2]);
 		});
+
+		it('Ne Devrait pas créer la room (Mauvais clientId)', function(){
+			var badPlayerId = undefined
+			master.createRoom(badPlayerId, { name: 'badId' }, cb);
+			badRoom = getGameFromPlayerId(badPlayerId, master)
+			expect(badRoom).to.be.undefined
+		})
+
+		it('Ne Devrait pas créer la room (Mauvais profil)', function(){
+			//Nom: empty name
+			master.createRoom(playersId[0], { name: "" }, cb);
+      expectNewRoom(getGameFromPlayerId(playersId[0], master), playersId[0]);
+			//Nom: name null
+			master.createRoom(playersId[0], { name: null }, cb);
+      expectNewRoom(getGameFromPlayerId(playersId[0], master), playersId[0]);
+			//Bad profil object (missing 'name')
+			master.createRoom(playersId[0], { pseudo: "Jean" }, cb);
+      expectNewRoom(getGameFromPlayerId(playersId[0], master), playersId[0]);
+
+		})
+
 		it('Devrait join la room ', () => {
+			//Player 1 rejoint la game crée par le joueur 0
 			master.joinRoom(playersId[1], players[1], room.getUrl(), cb);
-			expect(room.getListPlayers(playersId[1]).getId()).to.be.equal(playersId[1]);
-			expect(room.getListPlayers(playersId[1]).getProfil()).to.eql({
-				...players[1],
-				url: room.getUrl(),
-				owner: false
-      });
-
-      expect(room.getNbPlayer()).to.be.eql(2)
+			expectJoinRoom(room, playersId[1], players[1], 2);
+			//Player 2 rejoint la game crée par le joueur 0
 			master.joinRoom(playersId[2], players[2], room.getUrl(), cb);
-			expect(room.getListPlayers(playersId[2]).getId()).to.be.equal(playersId[2]);
-			expect(room.getListPlayers(playersId[2]).getProfil()).to.eql({
-				...players[2],
-				url: room.getUrl(),
-				owner: false
-			});
-      expect(room.getNbPlayer()).to.be.eql(3)
-
+			expectJoinRoom(room, playersId[2], players[2], 3);
 		});
 
-		// it('Ne Devrait pas créer le Tétriminos (Mauvais clientId)', function(){
-		//   expect(master.createRoom(0, { name: "Hector" }, cb)).to.exist
-		//   expect(master.createRoom(-543, { name: "Hector" }, cb)).to.exist
-		//   expect(master.createRoom(null, { name: "Hector" }, cb)).to.exist
-		// })
-		//
-		// it('Ne Devrait pas créer le Tétriminos (Mauvais profil)', function(){
-		//   expect(master.createRoom(472, { name: "" }, cb)).to.exist
-		//   expect(master.createRoom(472, { name: null }, cb)).to.exist
-		//   expect(master.createRoom(472, { pseudo: "Jean" }, cb)).to.exist
-		//   expect(master.createRoom(472, null, cb)).to.exist
-		// })
+		it('Ne devrait pas join la room (8 jooueurs déja présent)', () => {
+			var nbPlayer = 1
+			//crée une nouvelle game pour la remplir de joueur
+			master.createRoom(2100 + nbPlayer, { name: 'admin' }, cb);
+			room = getGameFromPlayerId(2100, master);
+			while (++nbPlayer <= 8) {
+				master.joinRoom(2100 + nbPlayer, { name: 'joueur' + nbPlayer }, room.getUrl(), cb);
+			}
+			var fullRoom = room;
+			//Essaye d'ajouter un joueur qui n'a pas de place dans la room
+			master.joinRoom(667, "joueur2trop", room.getUrl(), cb);
+			expect(getGameFromPlayerId(667, master)).to.be.undefined;
+			expect(room).to.be.eql(fullRoom);
+		});
+
+		it('Supprime 7 des 8 joueurs', () => {
+			//leaveRoom
+
+		}
+
+
+
+
+
+
 	});
 
 	// describe('[joinRoom] connection à la room crée', function(){
--- test/utils.mjs
+++ test/utils.mjs
@@ -1,4 +1,30 @@
 import Master from "../server/classes/Master.mjs"
+import { expect } from 'chai';
+import { defaultRules } from '../src/ressources/rules.mjs'
+
+const expectNewRoom = (room, playerId) => {
+  expect(room.isInGame()).to.be.eql(false);
+  expect(room).to.exist;
+  expect(room.getNbPlayer()).to.be.eql(1);
+  expect(room.getRules()).to.be.eql(defaultRules);
+  expect(room.getInterval()).to.be.eql(undefined);
+  expect(room.getShapes()).to.be.eql([]);
+  expect(room.getShapesId()).to.be.eql([]);
+  expect(room.getReadyToStart()).to.be.eql({});
+  expect(room.getReadyToStart()).to.be.eql({});
+  expect(room.getOwner()).to.be.eql(playerId);
+  expect(room.getArrivalOrder()).to.be.eql([ playerId ]);
+}
+
+const expectJoinRoom = (room, playerId, playerName, NbPlayer) =>  {
+  var player = room.getListPlayers(playerId)
+
+  expect(player.getId()).to.be.eql(playerId);
+  expect(player.getProfil()).to.eql({
+    ...playerName, url: room.getUrl(), owner: false
+  });
+  expect(room.getNbPlayer()).to.be.eql(NbPlayer);
+}
 
 const getGameFromPlayerId = (playerId, master) => {
   var allRooms = master.getRoomsList()
@@ -11,4 +37,4 @@ const getGameFromPlayerId = (playerId, master) => {
   return(undefined)
 }
 
-export { getGameFromPlayerId }
+export { expectNewRoom, expectJoinRoom, getGameFromPlayerId }
